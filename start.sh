88a7ac827ab21ab1bbcd170d61fb8778dd6e0e75
Eko Tri Rangkuti <kiretokai1@gmail.com>
2026-06-23 01:28:21 +0600

    Fix pulseaudio start command and add version checks

--- start.sh
+++ start.sh
@@ -2,26 +2,20 @@
 
 service dbus start
 
-
-pulseaudio \
---start \
+pulseaudio --start \
 --system \
 --disallow-exit \
 --disable-shm
 
-
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-
 service xrdp start
 
+echo "GLIBC:"
+ldd --version | head -1
 
-echo "===================="
-echo "XRDP READY"
 echo "Python:"
 python3 --version
-echo "===================="
-
 
 tail -f /var/log/xrdp-sesman.log
