9ec36f4b6131df0384a7112903361ca3a923e674
Eko Tri Rangkuti <kiretokai1@gmail.com>
2026-06-22 18:01:14 +0600

    Refactor start.sh and add Python version output
    Reformatted pulseaudio command and added version check for Python.

--- start.sh
+++ start.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+
+pulseaudio \
+--start \
+--system \
+--disallow-exit \
+--disable-shm
+
+
 service xrdp start
+
 
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
+
+
+echo "XRDP started"
+echo "Python version:"
+python3 --version
+
 
 tail -f /var/log/xrdp-sesman.log
