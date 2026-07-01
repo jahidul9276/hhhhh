a0e5d920a02b42319e87179f14d5d37aacbdc175
Eko Tri Rangkuti <kiretokai1@gmail.com>
2026-07-01 14:04:23 +0600

    Refactor start.sh for XRDP service initialization
    Updated start.sh to improve XRDP startup process and logging.

--- start.sh
+++ start.sh
@@ -3,23 +3,25 @@
 set -e
 
 mkdir -p /run/dbus
-dbus-daemon --system
-
+mkdir -p /var/run/xrdp
 mkdir -p /tmp/.X11-unix
+
 chmod 1777 /tmp/.X11-unix
 
-pulseaudio --system --disallow-exit --disable-shm &
+rm -f /tmp/.X*-lock
 
+dbus-daemon --system
+
+service xrdp-sesman start
 service xrdp start
 
-echo "======================"
-echo "GLIBC:"
-ldd --version | head -1
+echo "=============================="
+echo "XRDP Started"
+echo "Port : 3389"
+echo "User : root"
+echo "Password : ja908070"
+echo "=============================="
 
-echo "Python:"
-python3 --version
-
-echo "XRDP running on port 3389"
-echo "======================"
-
-tail -F /var/log/xrdp.log /var/log/xrdp-sesman.log
+tail -F \
+/var/log/xrdp.log \
+/var/log/xrdp-sesman.log
